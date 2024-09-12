15286a3cfef606ff56e7a60f8952c3bf24bc02a7
mikisz <mikolaj.niznik@gmail.com>
2024-09-13 00:20:51 +0200

    Simple, working logic

--- script.js
+++ script.js
@@ -1,156 +1,108 @@
-const screenResult = document.querySelector("#result");
-screenResult.textContent = '0';
+const display = document.querySelector("#result");
+const keyboard = document.querySelector(".keyboard")
 
-let firstNum;
-let secondNum;
-let activeOperator;
+let firstNum = null;
+let secondNum = null;
+let activeOperator = null;
 
-let activeNum = 'firstNum';
+display.textContent = 0;
 
-const acBTN = document.querySelector("#ac");
-acBTN.addEventListener("click", () => {
+keyboard.addEventListener("click", (event) => {
 
-    screenResult.textContent = '0';
-    firstNum = null;
-    secondNum = null;
-    activeNum = 'firstNum';
+    let target = event.target;
+
+    if (target.classList.contains("number")) {
+
+        if (activeOperator === null && firstNum === null) {
+
+            firstNum = target.textContent;
+            display.textContent = Number(firstNum);
+
+        } else if (activeOperator === null && firstNum !== null) {
+
+            firstNum += target.textContent;
+            display.textContent = Number(firstNum);
+
+        } else if (activeOperator !== null && secondNum === null) {
+
+            secondNum = target.textContent;
+            display.textContent = Number(secondNum);
+
+        } else if (activeOperator !== null && secondNum !== null) {
+
+            secondNum += target.textContent;
+            display.textContent = Number(secondNum);
+
+        }
+        
+
+    } else if (target.classList.contains("operator")) {
+
+        if (activeOperator === null) {
+
+            activeOperator = target.textContent;
+        
+        } else { operate(target.textContent, firstNum, secondNum); }
+        
+        console.log(activeOperator);
+
+    } else if (target.id === 'ac') {
+
+        firstNum = null;
+        secondNum = null;
+        activeOperator = null;
+
+        display.textContent = '0';
+
+    } else if (target.id === 'negative') {
+
+        // console.log(target.id);
+
+    } else if (target.id === 'percent') {
+
+        // console.log(target.id);
+
+    } else if (target.id === 'dot') {
+
+        // console.log(target.id);
+
+    } else if (target.id === 'getResult') {
+
+        if (firstNum !== null && activeOperator !== null) {
+            operate(activeOperator, firstNum, secondNum);
+        }
+
+    };
 
 })
 
-const negativeBTN = document.querySelector("#negative")
-negativeBTN.addEventListener("click", () => {
-    if (screenResult.textContent == '0') { 
+function operate(operator, x = 0, y = x) {
 
-    } else { 
-        let result = parseFloat(screenResult.textContent) * -1;
-        result = result.toString();
-        result = parseFloat(result);
-        screenResult.textContent = result.toString();    
-    }
-})
-
-
-const percentBTN = document.querySelector("#percent")
-percentBTN.addEventListener("click", () => {
-    let result = parseFloat(screenResult.textContent) * 0.01;
-    result = result.toString();
-    result = parseFloat(result);
-    screenResult.textContent = result.toString();    
-});
-
-const divideBTN = document.querySelector("#divide");
-divideBTN.addEventListener("click", () => operatorPress('divide'));
-
-const sevenBTN = document.querySelector("#seven");
-sevenBTN.addEventListener("click", () => numberPress('7'));
-
-const eightBTN = document.querySelector("#eight");
-eightBTN.addEventListener("click", () => numberPress('8'));
-
-const nineBTN = document.querySelector("#nine");
-nineBTN.addEventListener("click", () => numberPress('9'));
-
-const multiplyBTN = document.querySelector("#multiply");
-multiplyBTN.addEventListener("click", () => operatorPress('multiply'));
-
-const fourBTN = document.querySelector("#four");
-fourBTN.addEventListener("click", () => numberPress('4'));
-
-const fiveBTN = document.querySelector("#five");
-fiveBTN.addEventListener("click", () => numberPress('5'));
-
-const sixBTN = document.querySelector("#six");
-sixBTN.addEventListener("click", () => numberPress('6'));
-
-const subtractBTN = document.querySelector("#subtract");
-subtractBTN.addEventListener("click", () => operatorPress('subtract'));
-
-const oneBTN = document.querySelector("#one");
-oneBTN.addEventListener("click", () => numberPress('1'));
-
-const twoBTN = document.querySelector("#two");
-twoBTN.addEventListener("click", () => numberPress('2'));
-
-const threeBTN = document.querySelector("#three");
-threeBTN.addEventListener("click", () => numberPress('3'));
-
-const addBTN = document.querySelector("#add");
-addBTN.addEventListener("click", () => operatorPress('add'));
-
-const zeroBTN = document.querySelector("#zero");
-zeroBTN.addEventListener("click", () => numberPress(0));
-
-const dotBTN = document.querySelector("#dot");
-dotBTN.addEventListener("click", () => {
-
-    if (screenResult.textContent === "0" || screenResult.textContent.includes('.')) {
-
-    } else {
-        screenResult.textContent += '.';
-    }
-    
-});
-
-const resultBTN = document.querySelector("#getResult");
-resultBTN.addEventListener("click", () => {
-
-    if (firstNum === null || activeOperator === null) {
-
-    } else {
-        operate(activeOperator, firstNum, secondNum);
-    }
-
-});
-
-function numberPress(num) {
-    if (screenResult.textContent === '0') { screenResult.textContent = num;
-        } else { screenResult.textContent += num; }    
-};
-
-function operatorPress(operator) {
-    if (activeOperator == null) {
-        activeOperator = operator;
-        activeNum = 'secondNum';
-        firstNum = parseFloat(screenResult.textContent);
-        screenResult.textContent = '';
-    } else if (activeOperator !== null) {
-
-        operate(activeOperator, firstNum, secondNum);
-
-        activeOperator = operator;
-        activeNum = 'secondNum';
-    }
-};
-
-function operate(operator, firstNum, secondNum) {
-
-    a = firstNum;
-    b = secondNum ?? firstNum;
-    let sum = 0;
+    let sum;
 
     switch (operator) {
 
-        case 'divide':
-            sum = a / b;
-            break;
-        
-        case 'multiply':
-            sum = a * b;
+        case '/':
+            sum = x / y;
             break;
 
-        case 'subtract':
-            sum = a - b;
+        case '*':
+            sum = x * y;
             break;
 
-        case 'add':
-            sum = a + b;
+        case '-':
+            sum = x - y;
+            break;
+
+        case '+':
+            sum = x + y;
             break;
 
     }
 
-    screenResult.textContent = sum;
     firstNum = sum;
-    activeOperator = null;
     secondNum = null;
+    display.textContent = Number(firstNum);
+    activeOperator = null;
 
-};
+}
